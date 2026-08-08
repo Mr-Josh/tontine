b31e62509911015e56c01ca4774e63e75d15313b
Mr-Josh <jkimmich839@gmail.com>
2026-08-08 11:17:03 +0000

    chore: add shared domain types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,8 @@
+export type GroupId = string;
+export type UserId = string;
+export type MemberId = string;
+export type MeetingId = string;
+export type ContributionId = string;
+export type PaymentId = string;
+
+export type ContributionFrequency = 'daily' | 'weekly' | 'monthly';
